fd24418218aef5171c126764788fac2aa3fdff01
Myros <myros.net@gmail.com>
2022-12-15 18:07:55 -0500

    Init

--- variables.tf
+++ variables.tf
@@ -11,8 +11,6 @@ variable api_token_value {
 
 variable api_url {
   type        = string
-  # default = "https://rcwo9e1y09.execute-api.us-east-1.amazonaws.com/dev/api/v1/get-secret-id" # python code
-  default = "https://lqr59uko6i.execute-api.us-east-1.amazonaws.com/prod/get-secret-id" # Ruby Sinatra app
   description = "API URL"
 }
 
